bafd5287bf7c5c0daa2fd22996db12b05530d3cc
Marc Servat <marc.servat@smartcommerce.co>
2019-04-01 09:37:54 +0200

    Updates

---
+++ precache-manifest.d9d89f8e1663780bd4aa58085e9e3363.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1af2c6e761ce6106d607",
+    "url": "/albums/static/js/runtime~main.1af2c6e7.js"
+  },
+  {
+    "revision": "0f638c5d4153033d2f3e",
+    "url": "/albums/static/js/main.0f638c5d.chunk.js"
+  },
+  {
+    "revision": "df8d37e27c742e158a58",
+    "url": "/albums/static/js/2.df8d37e2.chunk.js"
+  },
+  {
+    "revision": "0a8fe999080253abebf86a2098c1afb2",
+    "url": "/albums/index.html"
+  }
+];
